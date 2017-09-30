75970118ee44b00d6068bc2c9d2baf1e2fb6ed60
Ernestas Venckus <e.venckus@ktu.edu>
2017-09-30 23:14:40 +0300

    Pradeta kurti controller'ius.

---
+++ E_Saldytuvas.Server/E_Saldytuvas.Server/Controllers/MeasuresController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using E_Saldytuvas.Server.Data;
+using E_Saldytuvas.Server.Models;
+
+namespace E_Saldytuvas.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class MeasuresController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public MeasuresController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+            if (_dbContext.Measures.Count() == 0)
+            {
+                _dbContext.Measures.Add(new Measure { Name = "Kg" });
+                _dbContext.SaveChanges();
+            }
+        }
+
+        // GET: api/measures
+        [HttpGet]
+        public IEnumerable<Measure> GetMeasures()
+        {
+            var measures = _dbContext.Measures
+                .ToList();
+            return measures;
+        }
+
+        // GET api/measures/5
+        [HttpGet("{measureId}", Name = "GetMeasure")]
+        public IActionResult GetMeasure(int measureId)
+        {
+            var measure = _dbContext.Measures.FirstOrDefault(m => m.Id == measureId);
+            if (measure == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(measure);
+        }
+
+        // POST api/measures
+        [HttpPost]
+        public IActionResult AddMeasure([FromBody] Measure measure)
+        {
+            if (measure == null)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Measures.Add(measure);
+            _dbContext.SaveChanges();
+
+            return CreatedAtRoute("GetMeasure", new { measureId = measure.Id }, measure);
+        }
+
+        // PUT api/measures/5
+        [HttpPut("{measureId}")]
+        public IActionResult UpdateMeasure(long measureId, [FromBody] Measure msr)
+        {
+            if (msr == null || msr.Id != measureId)
+            {
+                return BadRequest();
+            }
+
+            var measure = _dbContext.Measures.FirstOrDefault(m => m.Id == measureId);
+            if (measure == null)
+            {
+                return NotFound();
+            }
+
+            measure.Name = msr.Name;
+
+            _dbContext.Measures.Update(measure);
+            _dbContext.SaveChanges();
+            return new NoContentResult();
+        }
+
+        // DELETE api/measures/5
+        [HttpDelete("{measureId}")]
+        public IActionResult DeleteMeasure(int measureId)
+        {
+            var measure = _dbContext.Measures.FirstOrDefault(m => m.Id == measureId);
+            if (measure == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Measures.Remove(measure);
+            _dbContext.SaveChanges();
+            return new NoContentResult();
+        }
+    }
+}
--- E_Saldytuvas.Server/E_Saldytuvas.Server/Controllers/UsersController.cs
+++ E_Saldytuvas.Server/E_Saldytuvas.Server/Controllers/UsersController.cs
@@ -3,42 +3,99 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using E_Saldytuvas.Server.Data;
+using E_Saldytuvas.Server.Models;
 
 namespace E_Saldytuvas.Server.Controllers
 {
     [Route("api/[controller]")]
     public class UsersController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UsersController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+            if(_dbContext.Users.Count() == 0)
+            {
+                _dbContext.Users.Add(new User { Name = "Vardenis", Surname = "Pavardenis" });
+                _dbContext.SaveChanges();
+            }
+        }
+
         // GET api/users
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<User> GetUsers()
         {
-            return new string[] { "value1", "value2" };
+            var users = _dbContext.Users
+                .ToList();
+            return users;
         }
 
         // GET api/users/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{userId}", Name = "GetUser")]
+        public IActionResult GetUser(int userId)
         {
-            return "value";
+            var user = _dbContext.Users.FirstOrDefault(u => u.Id == userId);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(user);
         }
 
         // POST api/users
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult AddUser([FromBody] User user)
         {
+            if(user == null)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+
+            return CreatedAtRoute("GetUser", new { userId = user.Id }, user);
         }
 
         // PUT api/users/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        [HttpPut("{userId}")]
+        public IActionResult UpdateUser(long userId, [FromBody] User usr)
         {
+            if(usr == null || usr.Id != userId)
+            {
+                return BadRequest();
+            }
+
+            var user = _dbContext.Users.FirstOrDefault(u => u.Id == userId);
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            user.Name = usr.Name;
+            user.Surname = usr.Surname;
+
+            _dbContext.Users.Update(user);
+            _dbContext.SaveChanges();
+            return new NoContentResult();
         }
 
         // DELETE api/users/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete("{userId}")]
+        public IActionResult DeleteUser(int userId)
         {
+            var user = _dbContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Users.Remove(user);
+            _dbContext.SaveChanges();
+            return new NoContentResult();
         }
     }
 }
